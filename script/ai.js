acbe5125c0cb0ddb270aa48d66f1af34221a0b74
Sory Ronald <165383634+SoryRonald@users.noreply.github.com>
2024-08-09 19:56:42 +0000

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -4,7 +4,7 @@ module.exports.config = {
   version: '1.0.0',
   role: 0,
   hasPrefix: false,
-  aliases: ['gpt', 'openai', 'assistant'],
+  aliases: ['gpt', 'openai'],
   description: "An AI command powered by GPT-4",
   usage: "Ai [promot]",
   credits: 'Metoushela Walker',
@@ -17,17 +17,17 @@ module.exports.run = async function({
 }) {
   const input = args.join(' ');
   if (!input) {
-    api.sendMessage(`𝙎𝙖𝙡𝙪𝙩 𝙟𝙚 𝙨𝙪𝙞𝙨 𝙪𝙣𝙚 𝙞𝙣𝙩𝙚𝙡𝙡𝙞𝙜𝙚𝙣𝙘𝙚 𝙖𝙧𝙩𝙞𝙛𝙞𝙘𝙞𝙚𝙡𝙡𝙚 𝙚𝙩 𝙟𝙚 𝙧𝙚𝙥𝙤𝙣𝙙 à 𝙩𝙤𝙪𝙩𝙚𝙨 𝙡𝙚𝙨 𝙦𝙪𝙚𝙨𝙩𝙞𝙤𝙣𝙨`, event.threadID, event.messageID);
+    api.sendMessage(`𝙌𝙪𝙚𝙡𝙡𝙚 𝙚𝙨𝙩 𝙫𝙤𝙩𝙧𝙚 𝙦𝙪𝙚𝙨𝙩𝙞𝙤𝙣 ☺?`, event.threadID, event.messageID);
     return;
   }
-  api.sendMessage(`☘️𝚂ｈ𝚎𝚊ｒ𝚌ｈ...\n━━━━━━━━━━━\n "${input}"`, event.threadID, event.messageID);
+  api.sendMessage(`💬 𝗚𝗣𝗧-4\n━━━━━━━━━━━━━━━━\n "${input}"`, event.threadID, event.messageID);
   try {
     const {
       data
     } = await axios.get(`https://metoushela-rest-api-tp5g.onrender.com/api/gpt4o?context=${encodeURIComponent(input)}`);
     const response = data.response;
-    api.sendMessage('⚘𝗔𝘀𝘀𝗶𝘀𝘁𝗮𝗻𝘁⊰♔⊱\n\n⊰⊹⊱♡⊰⊹⊱♡⊰⊹⊱♡⊰⊹\n' + response + '\n╰┈➤⊹⊱✫⊰⊹⊱✫⊰🍀', event.threadID, event.messageID);
+    api.sendMessage( '💬 𝗚𝗣𝗧-4\n━━━━━━━━━━━━━━━━\n' + response + '\n━━━━━━━━━━━━━━━━\n', event.threadID, event.messageID);
   } catch (error) {
-    api.sendMessage('🔴 An error occurred while processing your request..', event.threadID, event.messageID);
+    api.sendMessage('An error occurred while processing your request.', event.threadID, event.messageID);
   }
 };
